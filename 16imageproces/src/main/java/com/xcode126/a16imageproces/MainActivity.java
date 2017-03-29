211b78eaddc04bbcd534f84f6c5165919ea0972d
xcode126 <xcode126@126.com>
2017-03-29 10:39:21 +0800

    增加图像处理Demo

---
+++ 16imageproces/src/main/java/com/xcode126/a16imageproces/MainActivity.java
@@ -0,0 +1,20 @@
+package com.xcode126.a16imageproces;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 作者：sky
+ * 邮箱：xcode126@126.com
+ * QQ:1397028339
+ * 公众号：走近程序员
+ * 作用：安卓图像的处理
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
